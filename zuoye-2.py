c4fda515de31692a2c28ada00ee2d9f96c4fcecb
zhuhaoyue <1246334366@qq.com>
2021-10-23 17:24:58 +0800

    第二章作业

---
+++ zuoye-2.py
@@ -0,0 +1,8 @@
+print("Life is short,you need python.")
+print("What's your name?")
+'''
+    This is my first program.
+    I will be a great programmer.
+'''
+print("hello,world")        #print English word.
+print("世界，你好")         #print Chinese word.
